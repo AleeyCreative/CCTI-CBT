74a5f77aef7e052d4fc6b4571da2a72e230ff795
Baba Ali AbdulAziz <frontdev4@stableshield.com>
2021-11-17 07:57:25 +0000

    chore: Minor cleanup and build

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-import express from 'express';
-import path from 'path';
+const express = require('express')
+const path = require('path')
 
 const app = express();
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   app.use(express.static('/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/', 'build', 'index.html'));
+  app.get('/3rd-party-licenses.txt', (req,res) => {
+    res.sendFile(path.join(__dirname,'/', 'build', '3rd-party-licenses.txt'));
   })
-}
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  })
+// }
 
 const port = process.env.PORT || 5000;
 
